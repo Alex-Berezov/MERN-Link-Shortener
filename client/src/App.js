45ccc11b98599d203636b53eb69558351c2dff9c
Alex Berezov <53948974+Alex-Berezov@users.noreply.github.com>
2021-02-10 19:48:15 +0400

    Added React + Materialize-css

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import 'materialize-css'
+
+function App() {
+    return (
+        <div className="container">
+            <h1>Hello</h1>
+            <a className="waves-effect waves-light btn purple darken-1">button</a>
+        </div>
+    )
+}
+
+export default App
