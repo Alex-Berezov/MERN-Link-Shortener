71b31332f0d9f77f8ee0fd035d720ec5adf7805b
Alex Berezov <53948974+Alex-Berezov@users.noreply.github.com>
2021-02-19 13:36:55 +0400

    Edited app.js file

--- app.js
+++ app.js
@@ -6,20 +6,26 @@ const mongoose = require('mongoose')
 const app = express()
 
 app.use(express.json({ extended: true }))
+app.use(express.static(path.join(__dirname, 'client/build')));
 
 app.use('/api/auth', require('./routes/auth.routes'))
 app.use('/api/link', require('./routes/link.routes'))
 app.use('/t', require('./routes/redirects.routs'))
 
-if (process.env.NODE_ENV === 'production') {
-    app.use('/', express.static(path.join(__dirname, 'client', 'build')))
+// if (process.env.NODE_ENV === 'production') {
+//     app.use('/', express.static(path.join(__dirname, 'client', 'build')))
+//
+//     app.get('*', (req, res) => {
+//         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+//     })
+// }
 
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-    })
-}
+app.get('*', (req,res) =>{
+    res.sendFile(path.join(__dirname+'/client/build/index.html'));
+});
 
-const PORT = config.get('port') || 5000
+//const PORT = config.get('port') || 5000
+const PORT = process.env.PORT || 5000
 
 async function start () {
     try {
